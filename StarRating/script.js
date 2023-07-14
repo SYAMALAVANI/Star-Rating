022ae8add59970aeb7590e48df4897e44ac25524
SYAMALAVANI <78420988+SYAMALAVANI@users.noreply.github.com>
2023-07-15 01:18:36 +0530

    Add files via upload

---
+++ StarRating/script.js
@@ -0,0 +1,55 @@
+const smileys = ['😢', '😞', '😐', '😀', '😎'];
+const smileyRef = document.querySelector('.smiley');
+const ratingSectionRef = document.getElementById('rating-section');
+
+let hoveredValue = 0;
+let selectedStarValue = 0;
+
+function createStars() {
+  smileys.forEach((item, idx) => {
+    const newStarElementRef = document.createElement('div');
+    newStarElementRef.classList.add('rating-box', 'empty-star');
+    newStarElementRef.setAttribute('data-id', idx + 1);
+    ratingSectionRef.appendChild(newStarElementRef);
+  });
+}
+
+ratingSectionRef.addEventListener('mouseover', function (ev) {
+  if (ev.target.classList.contains('rating-box')) {
+    const currentStarIndex = ev.target.dataset.id;
+    hoveredValue = currentStarIndex;
+    rerenderStars();
+  }
+});
+
+ratingSectionRef.addEventListener('mouseleave', function () {
+  hoveredValue = 0;
+  rerenderStars();
+});
+
+ratingSectionRef.addEventListener('click', function (ev) {
+  if (ev.target.classList.contains('rating-box')) {
+    const countSpanRef = document.getElementById('count');
+    const currentStarIndex = ev.target.dataset.id;
+    selectedStarValue = currentStarIndex;
+    rerenderStars();
+    smileyRef.innerText = smileys[selectedStarValue - 1];
+    countSpanRef.textContent = selectedStarValue;
+  }
+});
+
+function rerenderStars() {
+  const ratingBoxes = document.querySelectorAll('.rating-box');
+  [...ratingBoxes].forEach((boxRef, idx) => {
+    const highlightIndex = hoveredValue || selectedStarValue;
+    if (highlightIndex > idx) {
+      boxRef.classList.add('fulled-star');
+      boxRef.classList.remove('empty-star');
+    } else {
+      boxRef.classList.remove('fulled-star');
+      boxRef.classList.add('empty-star');
+    }
+  });
+}
+
+createStars();
